973fb6282b1ce26ed6b283ed80063ff00a279fb0
jdurham2843 <jdurham2843@gmail.com>
2016-06-18 15:43:50 -0500

    Added Deck model, began unit test for creating Decks

--- flashcards/models.py
+++ flashcards/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Deck(models.Model):
+    title = models.CharField(max_length=50)
+    cardCount = models.IntegerField(default=0)
--- flashcards/tests.py
+++ flashcards/tests.py
@@ -2,9 +2,16 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from flashcards.views import home_page
 from django.http import HttpRequest
+from flashcards.models import Deck
 
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+class DeckTests(TestCase):
+
+    def test_adding_a_new_deck(self):
+        self.assertFalse(Deck.objects.all())
+        # add_new_Deck(request, content)
