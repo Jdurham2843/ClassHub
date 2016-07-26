837a254c799e7a1ac512ba8ea6cb392ffac29ba4
jdurham2843 <jdurham2843@gmail.com>
2016-07-25 19:40:54 -0500

    Made changes for heroku

--- classHub/classHub/settings.py
+++ classHub/classHub/settings.py
@@ -125,6 +125,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-#STATIC_ROOT = os.path.join((BASE_DIR), 'static')
+STATIC_ROOT = os.path.join((BASE_DIR), 'flashcards/static')
 
 LOGIN_URL = '/login/'
